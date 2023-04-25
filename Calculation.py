715a8020ce4169939d5cf879470d88e1d0f07889
ShreyNav <shreyshi.navneet@gmail.com>
2023-04-26 00:08:23 +0100

    changed code

---
+++ Calculation.py
@@ -0,0 +1,8 @@
+def add_num(a,b):
+    return float(a) + float(b)
+
+
+a = input('Enter value for first number ---->')
+b = input('Enter value for second number---->')
+result= add_num(a, b)
+print('\n Result', result)
---
+++ Tests/test.py
@@ -0,0 +1,5 @@
+from ..Calculation import add_num
+
+def test_add_num():
+    assert add_num(2, 3) == 5.0
+    assert add_num(1, 2) == 3.0
